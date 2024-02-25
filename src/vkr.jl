2bb13efc64692da08943fd8cfb07b430121d21bc
Artyom Abramyan <artemabramyan7@icloud.com>
2024-02-25 14:28:34 +0300

    Put everything into module

---
+++ src/vkr.jl
@@ -0,0 +1,16 @@
+module VKR
+
+import StatsBase
+import Random
+using Random: default_rng
+import Memoize
+import Graphs
+using Transducers
+
+
+include("utils.jl")
+include("ga.jl")
+include("operators.jl")
+include("graph_coloring_problem.jl")
+
+end
