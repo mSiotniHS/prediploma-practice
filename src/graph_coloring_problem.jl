024f098fe372ab4009eca1c92d125be6154b1730
Artyom Abramyan <artemabramyan7@icloud.com>
2024-05-30 21:38:10 +0300

    Change comments, add functions

--- src/graph_coloring_problem.jl
+++ src/graph_coloring_problem.jl
@@ -1,4 +1,4 @@
-### TYPES
+#region TYPES
 
 const Vertex = Int
 const Color = Int
@@ -6,10 +6,10 @@ const Coloring = Vector{Color}
 const SomeColoring = Vector{Union{Color, Nothing}}
 const SMALLEST_COLOR = 0::Color
 
-### end TYPES
+#endregion
 
 
-### CRITERIONS
+#region CRITERIONS
 
 color_count(coloring::Coloring) = length(unique!(sort(coloring)))
 
@@ -33,10 +33,10 @@ function color_count_with_penalty(graph::Graphs.AbstractGraph; weight = 1)
     fitness
 end
 
-### end CRITERIONS
+#endregion
 
 
-### POPULATION GENERATORS
+#region POPULATION GENERATORS
 
 function random_coloring(graph::Graphs.AbstractGraph; rng::Random.AbstractRNG=default_rng())
     vertices = Graphs.vertices(graph)
@@ -57,7 +57,7 @@ function any_valid_color(graph::Graphs.AbstractGraph, vertex::Vertex, coloring::
     rand(rng, valid_colors)
 end
 
-get_non_nothing_colors(vertices::Vector{Vertex}, coloring::SomeColoring) =
+get_non_nothing_colors(vertices::Vector{Vertex}, coloring::Union{Coloring, SomeColoring}) =
     vertices |>
         Map(vertex -> coloring[vertex]) |>
         NotA(Nothing) |>
@@ -76,11 +76,11 @@ greedy_coloring(ordering_strategy) = (graph::Graphs.AbstractGraph) -> begin
     coloring
 end
 
-function smallest_color(graph::Graphs.AbstractGraph, vertex::Vertex, coloring::Coloring)
+function smallest_color(graph::Graphs.AbstractGraph, vertex::Vertex, coloring::Union{Coloring, SomeColoring})
     smallest_color(Graphs.neighbors(graph, vertex), coloring)
 end
 
-function smallest_color(neighbours::Vector{Vertex}, coloring::Coloring)
+function smallest_color(neighbours::Vector{Vertex}, coloring::Union{Coloring, SomeColoring})
     neighbour_colors = get_non_nothing_colors(neighbours, coloring)
 
     if isempty(neighbour_colors)
@@ -89,6 +89,8 @@ function smallest_color(neighbours::Vector{Vertex}, coloring::Coloring)
 
     sort!(neighbour_colors)
 
+    @info "neighbour colors are"  neighbour_colors
+
     color = SMALLEST_COLOR
 
     for i in eachindex(neighbour_colors)
@@ -96,11 +98,13 @@ function smallest_color(neighbours::Vector{Vertex}, coloring::Coloring)
         color += 1
     end
 
+    @info "Smallest acceptable is"  color
+
     color
 end
 
 function randomized_lf_ordering(graph::Graphs.AbstractGraph)
-    vertices = Graphs.vertices(graph)
+    vertices = tcollect(Graphs.vertices(graph))
     degrees = vertices |> Map(vertex -> Graphs.degree(graph, vertex)) |> tcollect
 
     ordering = Vector{Int}()
@@ -114,6 +118,10 @@ function randomized_lf_ordering(graph::Graphs.AbstractGraph)
     end
 
     ordering
+end
+
+function lf_ordering(graph::Graphs.AbstractGraph)
+    sort(Graphs.vertices(graph), by=(vertex -> Graphs.degree(graph, vertex)), rev=true)
 end
 
 # function randomized_sl_ordering(graph)
@@ -138,4 +146,53 @@ end
 
 random_ordering(graph::Graphs.AbstractGraph) = Random.shuffle!(tcollect(Graphs.vertices(graph)))
 
-### end POPULATION GENERATORS
+#endregion
+
+
+#region GRAPH GENERATION UTILITIES
+
+function generate_vertex_degrees(μ::Float64, σ::Float64, count::Int)
+    values = round.(Int, μ .+ Random.randn(count) .* σ)
+
+    if isodd(sum(values))
+        values[Random.rand(1:length(values))] += Random.rand(Bool) ? 1 : -1
+    end
+
+    return values
+end
+
+itemgetter(i::Int) = x -> x[i]
+itemgetter_2 = itemgetter(2)
+
+function generate_graph_with_vertex_degrees(vertex_degrees::Vector{Int})
+    vertex_count = length(vertex_degrees)
+    graph = Graphs.SimpleGraph(vertex_count)
+    vertex_degrees = vertex_degrees |> Enumerate() |> collect
+
+    while !isempty(vertex_degrees)
+        sort!(vertex_degrees, by=itemgetter_2, rev=true)
+
+        vertex, degree = vertex_degrees[1]
+        for i in 2:(degree+1)
+            Graphs.add_edge!(graph, vertex, vertex_degrees[i][1])
+            vertex_degrees[i] = (vertex_degrees[i][1], vertex_degrees[i][2] - 1)
+        end
+
+        deleteat!(vertex_degrees, 1)
+    end
+
+    return graph
+end
+
+#endregion
+
+
+#region Misc
+function check_coloring(graph::Graphs.AbstractGraph, coloring::Coloring)
+    for edge in Graphs.edges(graph)
+        if coloring[Graphs.src(edge)] == coloring[Graphs.dst(edge)]
+            @warn "Found error!"
+        end
+    end
+end
+#endregion
